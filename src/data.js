87098a295e590da7e307cc5365ed7b8fbcafde28
nbb02 <berresn050@gmail.com>
2024-03-02 19:31:46 +0800

    first commit

---
+++ src/data.js
@@ -0,0 +1,100 @@
+const galleryData = [
+  {
+    type: "Cubism",
+    img: "/images/gallery/cubism.jpg",
+  },
+  {
+    type: "Abstract",
+    img: "/images/gallery/abstract.jpg",
+  },
+  {
+    type: "Pop",
+    img: "/images/gallery/popart.jpg",
+  },
+  {
+    type: "Surrealism",
+    img: "/images/gallery/surrealism.jpg",
+  },
+  {
+    type: "Tempera",
+    img: "/images/gallery/tempera.jpg",
+  },
+  {
+    type: "Gouache",
+    img: "/images/gallery/gouache.jpg",
+  },
+  {
+    type: "Pastel",
+    img: "/images/gallery/pastel.jpg",
+  },
+  {
+    type: "Portraiture",
+    img: "/images/gallery/portraiture.jpg",
+  },
+  {
+    type: "Miniature",
+    img: "/images/gallery/miniature.jpg",
+  },
+  {
+    type: "Impressionism",
+    img: "/images/gallery/impressionism.jpg",
+  },
+  {
+    type: "Conceptual",
+    img: "/images/gallery/conceptual.jpg",
+  },
+  {
+    type: "Fresco",
+    img: "/images/gallery/fresco.jpg",
+  },
+]
+
+const artistData = [
+  { name: "Vincent van Gogh", img: "" },
+  { name: "Pablo Picasso", img: "" },
+  { name: "Michelangelo", img: "" },
+  { name: "Leonardo da Vinci", img: "" },
+  { name: "Rembrandt", img: "" },
+  { name: "Salvador Dali", img: "" },
+  { name: "Frida Kahlo", img: "" },
+  { name: "Henri Matisse", img: "" },
+  { name: "Claude Monet", img: "" },
+  { name: "Andy Warhol", img: "" },
+  { name: "Edvard Munch", img: "" },
+  { name: "Johannes Vermeer", img: "" },
+]
+
+const aboutUsData = [
+  {
+    name: "Ryan Alcober",
+    img: "/images/aboutUs/Alcober, Ryan.jpg",
+    role: "Project Manager",
+    info: "The project manager oversees planning, execution, and completion of tasks, ensuring alignment with objectives and timelines, and addressing any challenges to keep the project on track.",
+  },
+  {
+    name: "Nathaniel B. Berres",
+    img: "/images/aboutUs/Berres, Nathaniel.png",
+
+    role: "Lead Programmer",
+    info: "The lead programmer directs the technical aspects of software development, organizing tasks, assigning roles, and ensuring code quality and efficiency. They collaborate with team members, troubleshoot issues, and strive for innovation to meet project goals effectively.",
+  },
+  {
+    name: "Mark Trojan T. Manikat",
+    img: "",
+    role: "Prototype Designer",
+    info: "The prototype designer collaborates with teams to conceptualize and create early-stage models of products or systems. They refine ideas, iterate on prototypes, and validate concepts to ensure user-centric design and innovation in product development.",
+  },
+  {
+    name: "Angelica L. Evangelista",
+    img: "/images/aboutUs/Evangelista, Angelica.jpeg",
+    role: "Lead Researcher",
+    info: "The lead researcher oversees the research process, guiding team members in gathering, analyzing, and interpreting data. They establish research goals, methodologies, and timelines, ensuring accuracy and relevance of findings to inform decision-making and advance knowledge in the field.",
+  },
+  {
+    name: "Patrica Ann N. Castro",
+    img: "",
+    role: "Assistant Designer and Researchers",
+    info: "The assistant supports various tasks within an organization, aiding in administrative duties, scheduling, and correspondence. They assist team members, manage documents, and contribute to the smooth functioning of operations by providing valuable support across different areas of responsibility.",
+  },
+]
+export { galleryData, artistData, aboutUsData }
